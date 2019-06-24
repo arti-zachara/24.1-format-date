01a494da88e20861e863f8ba46b5b99b1be68163
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-06-24 11:12:01 +0200

    Fix formatDate to pass all tests (changed to strictEqual)

--- index.test.js
+++ index.test.js
@@ -8,7 +8,7 @@ describe("formatDate", () => {
 
       const expected = "23s";
 
-      assert.equal(actual, expected);
+      assert.strictEqual(actual, expected);
     });
 
     it("should return 0 seconds if no value is passed", () => {
@@ -16,7 +16,7 @@ describe("formatDate", () => {
 
       const expected = "0s";
 
-      assert.equal(actual, expected);
+      assert.strictEqual(actual, expected);
     });
   });
 
@@ -26,7 +26,7 @@ describe("formatDate", () => {
 
       const expected = "1m 5s";
 
-      assert.equal(actual, expected);
+      assert.strictEqual(actual, expected);
     });
 
     it("should work for 2 digits minutes and seconds", () => {
@@ -34,7 +34,7 @@ describe("formatDate", () => {
 
       const expected = "23m 34s";
 
-      assert.equal(actual, expected);
+      assert.strictEqual(actual, expected);
     });
 
     it("should return only minutes", () => {
@@ -42,7 +42,7 @@ describe("formatDate", () => {
 
       const expected = "13m";
 
-      assert.equal(actual, expected);
+      assert.strictEqual(actual, expected);
     });
   });
 
@@ -52,7 +52,7 @@ describe("formatDate", () => {
 
       const expected = "1h 2m 3s";
 
-      assert.equal(actual, expected);
+      assert.strictEqual(actual, expected);
     });
 
     it("should work for 2 digit hours, minutes and seconds", () => {
@@ -60,7 +60,7 @@ describe("formatDate", () => {
 
       const expected = "11h 12m 13s";
 
-      assert.equal(actual, expected);
+      assert.strictEqual(actual, expected);
     });
 
     it("should work for hours only", () => {
@@ -68,7 +68,7 @@ describe("formatDate", () => {
 
       const expected = "2h";
 
-      assert.equal(actual, expected);
+      assert.strictEqual(actual, expected);
     });
 
     it("should work for hours and minutes", () => {
@@ -76,7 +76,7 @@ describe("formatDate", () => {
 
       const expected = "21h 37m";
 
-      assert.equal(actual, expected);
+      assert.strictEqual(actual, expected);
     });
 
     it("should work for hours and seconds", () => {
@@ -84,7 +84,7 @@ describe("formatDate", () => {
 
       const expected = "3h 15s";
 
-      assert.equal(actual, expected);
+      assert.strictEqual(actual, expected);
     });
   });
 });
